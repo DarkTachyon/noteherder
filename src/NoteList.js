ae9c4489978902698c00c5458191e1099b3911e6
DarkTachyon <tmb.alexander@gmail.com>
2017-07-05 15:47:30 -0400

    Complete Sidebar, NoteList, and NoteForm and add props for note

--- src/NoteList.js
+++ src/NoteList.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
 import './NoteList.css'
+import Note from './Note'
 
 const NoteList = () => {
-    return <p>NoteList!</p>
+    const note = {
+        id: 'note-4',
+        title: 'My fancy note',
+        body: 'Oh, so fancy, I do declare!',
+    }
+    return (
+        <div className="NoteList">
+          <h3>Notes</h3>
+          <ul id="notes">
+            <Note note={ note }/>
+          </ul>
+        </div>
+    )
 }
 
 export default NoteList
