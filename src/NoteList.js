9b5c3f07d39642926ac853a51771a1c1dcc4fef1
DarkTachyon <tmb.alexander@gmail.com>
2017-07-05 15:50:50 -0400

    Dynamically assign notes from an array

--- src/NoteList.js
+++ src/NoteList.js
@@ -4,15 +4,18 @@ import './NoteList.css'
 import Note from './Note'
 
 const NoteList = () => {
-    const note = {
-        id: 'note-4',
-        title: 'My fancy note',
-        body: 'Oh, so fancy, I do declare!',
-    }
+    const note = [
+        {
+            id: 'note-4',
+            title: 'My fancy note',
+            body: 'Oh, so fancy, I do declare!',
+        }
+    ]
     return (
         <div className="NoteList">
           <h3>Notes</h3>
           <ul id="notes">
+            { notes.map(note => return <Note note={ note } />) }
             <Note note={ note }/>
           </ul>
         </div>
